bcd0264d12094c9ab1ab8ff1934b800399e853cc
Szymon1932 <szymon.kordal@student.wat.edu.pl>
2022-12-05 13:58:34 +0100

    Update (without checking inputs) + deleting

--- farm_helper/fields/urls.py
+++ farm_helper/fields/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_element/', views.add_fertilizer, name='add-fertilizer'),
-    path('update/<int:book_id>', views.update_fertilizer),
-    path('delete/<int:book_id>', views.delete_fertilizer)
+    path('update/<int:fertilizer_id>', views.update_fertilizer),
+    path('delete/<int:fertilizer_id>', views.delete_fertilizer)
 ]
