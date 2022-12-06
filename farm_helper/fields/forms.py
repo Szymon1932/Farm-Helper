897695254b0e042dd394f67d6224ce8f038445c9
Szymon1932 <szymon.kordal@student.wat.edu.pl>
2022-12-06 10:57:56 +0100

    Added view (and temporary edition/deletion) of classfields

--- farm_helper/fields/forms.py
+++ farm_helper/fields/forms.py
@@ -7,3 +7,17 @@ class CreateFertilizer(forms.ModelForm):
     class Meta:
         model = Fertilizer
         fields = '__all__'
+
+
+class CreateClassField(forms.ModelForm):
+
+    class Meta:
+        model = ClassField
+        fields = '__all__'
+
+
+class CreateFertilizer(forms.ModelForm):
+
+    class Meta:
+        model = Fertilizer
+        fields = '__all__'
--- farm_helper/fields/urls.py
+++ farm_helper/fields/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import viewsFertilizer
+from .views import viewsFertilizer, viewsClassField
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -12,6 +12,16 @@ urlpatterns = [
     path('fertilizers/update/<int:fertilizer_id>',
          viewsFertilizer.update_fertilizer),
     path('fertilizers/delete/<int:fertilizer_id>',
-         viewsFertilizer.delete_fertilizer)
+         viewsFertilizer.delete_fertilizer),
 
+    # class_fields
+
+    path('class_fields/', viewsClassField.show_class_fields,
+         name='show-class_fields'),
+    path('class_fields/add_element/',
+         viewsClassField.add_class_field, name='add-class_field'),
+    path('class_fields/update/<int:class_field_id>',
+         viewsClassField.update_class_field),
+    path('class_fields/delete/<int:class_field_id>',
+         viewsClassField.delete_class_field),
 ]
