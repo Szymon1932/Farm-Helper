e261ac8801a2bc33e89353fdb61c16ec5319053e
Szymon1932 <szymon.kordal@student.wat.edu.pl>
2022-12-08 11:06:20 +0100

    Datepicker in plantprice

--- farm_helper/fields/forms.py
+++ farm_helper/fields/forms.py
@@ -25,11 +25,16 @@ class CreatePlant(forms.ModelForm):
 DATE_INPUT_FORMATS = ('%d/%m/%Y', '%d-%m-%Y', '%Y-%m-%d')
 
 
+class DatePickerInput(forms.DateInput):
+    input_type = 'date'
+
+
 class CreatePlantPrice(forms.ModelForm):
-    date = forms.DateField(widget=forms.DateInput(
-        format='%d/%m/%Y'), input_formats=DATE_INPUT_FORMATS)  # add better formatting and filerting
+    # date = forms.DateField(widget=forms.DateInput(format='%d/%m/%Y'), input_formats=DATE_INPUT_FORMATS)  # add better formatting and filerting
+    date = forms.DateField(widget=DatePickerInput)
     # add retrieving name not key
 
     class Meta:
         model = PlantPrice
         fields = '__all__'
+        #fields = ['price','date','plant','is_predicted']
